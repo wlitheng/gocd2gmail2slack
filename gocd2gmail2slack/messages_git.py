3b2a7fa6ebbec89849c654dc7554c0ddad31267e
Ryan Williams <ryan.williams@nintex.com>
2017-08-10 13:50:53 +0800

    Fixed Git email message parsing.  Fixed unit tests to exclude login name 'xyz@' prefix from changeset URL.

--- gocd2gmail2slack/messages_git.py
+++ gocd2gmail2slack/messages_git.py
@@ -5,14 +5,14 @@ import pdb
 GOCD_PATTERN = (r"Stage\s*\[(\S*)\/\d*\/(\S*)\/\d*\]\s*"
                 r"(passed|failed|is fixed|is broken)")
                 
-BASE_GIT_SSH_PATTERN = r"Git: (ssh:\/\/.*?)\\r"
+BASE_GIT_SSH_PATTERN = r"Git: ssh:\/\/(?:.*?@)?(.*?)\\r"
 
 REVISION_PATTERN = (r"revision: (\w+), "
-                    r"modified by \w+\\\\(\w+) "
+                    r"modified by (.*?) "
                     r"\<(.*?)\> "
                     r"on (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+)\s*"
-                    r"([\s\S]*)\s*modified")
-        
+                    r"([\s\S]*?)\s*modified ")
+					
 def get_subject(message):
     for header in message['payload']['headers']:
         if header['name'] == 'Subject':
@@ -55,8 +55,8 @@ def get_changeset_url(body):
     match = re.search(BASE_GIT_SSH_PATTERN, body)
     if match:
         base_url = match.group(1)
-        #replace ssh with https
-        base_url = base_url.replace("ssh:", "https:")
+        #prepend https://
+        base_url = "https://" + base_url
         #replace :22 with nothing
         base_url = base_url.replace(":22", "")
         return base_url + "/commit/" + get_changeset_id(body) 
@@ -65,14 +65,13 @@ def get_changeset_id(body):
     match = re.search(REVISION_PATTERN, body)
     if match:
         return match.group(1)
-    
+
 def get_changeset_comment(body):
     match = re.search(REVISION_PATTERN, body)
     if match:
         first_pass = match.group(5)
-        second_pass = first_pass.split('modified $/', 1)[0]
-        third_pass = second_pass.replace("\\n", "").replace("\\r", "")
-        return third_pass.strip()
+        second_pass = first_pass.replace("\\n", "").replace("\\r", "")
+        return second_pass.strip()
 
 def get_changeset_author(body):
     match = re.search(REVISION_PATTERN, body)
